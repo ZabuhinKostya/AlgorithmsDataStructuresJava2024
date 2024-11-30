ee2868bca05cc667959ae44492b2667f830c7732
ZabuhinKostya <kostiantyn.zabuhin@stud.onu.edu.ua>
2024-11-30 21:42:45 +0200

    Hintprovider

--- ConsoleGame/Game/HintProvider.java
+++ ConsoleGame/Game/HintProvider.java
@@ -1,16 +1,5 @@
 package ConsoleGame.Game;
 
-class HintProvider {
-    private int lowerBound = 1;
-    private int upperBound = 100;
-
-    public String getHint(int lastGuess, int secretNumber) {
-        if (lastGuess < secretNumber) {
-            lowerBound = Math.max(lowerBound, lastGuess + 1);
-            return "Загаданное число больше. Попробуйте диапазон: " + lowerBound + " - " + upperBound;
-        } else {
-            upperBound = Math.min(upperBound, lastGuess - 1);
-            return "Загаданное число меньше. Попробуйте диапазон: " + lowerBound + " - " + upperBound;
-        }
-    }
+interface HintProvider {
+    String getHint(int lastGuess, int secretNumber);
 }
